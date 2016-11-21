8ba50721a58cacb9515f752498e89295f1125066
Kazauwa <i.heart.d.dead@gmail.com>
2016-11-21 14:17:12 +0300

    added user input

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -11,3 +11,18 @@ def get_roots(a, b, c):
         return root1, None
     else:
         return root1, root2
+
+
+if __name__ == '__main__':
+    print('Hi! I can solve quadratic equations!')
+    while True:
+        try:
+            a = int(input('Input first coefficent (a): '))
+            b = int(input('Input second coefficent (b): '))
+            c = int(input('Input last coefficent (c): '))
+        except ValueError:
+            print('\nCoefficent must be number only!\n')
+            continue
+        root1, root2 = get_roots(a, b, c)
+        print('Roots of your equation are {0} and {1}'.format(root1, root2))
+        break
